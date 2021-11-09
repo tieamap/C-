21c8660586d0b035c9f54df4b39a2d9bd7df3aee
tieamap <tieamap@gmail.com>
2021-11-09 21:41:02 +0900

    * P119迄の内容で作成したソースを一括でコミット ** 書籍名:Visual C# 2015 パーフェクトマスター ** 出版社:秀和システム

---
+++ CalculateApp/CalculateApp/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculateApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int price, quantity, subtotal, tax, total;
+            const double TAXR_RATE = 0.08;
+
+            // テキストボックスに入力された値は全て文字列扱いになるので型変換が必要
+            price = Convert.ToInt32(textBox1.Text);
+            quantity = Convert.ToInt32(textBox2.Text);
+            subtotal = price * quantity;
+            tax = (int)(subtotal * TAXR_RATE);
+
+            total = subtotal + tax;
+
+            label6.Text = Convert.ToString(subtotal);
+            label7.Text = Convert.ToString(tax);
+            label8.Text = Convert.ToString(total);
+        }
+    }
+}
