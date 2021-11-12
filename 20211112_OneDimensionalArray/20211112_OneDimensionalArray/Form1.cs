d3a851f4c94a01f9c89462cda69ee89650d3c88c
tieamap <tieamap@gmail.com>
2021-11-12 16:04:20 +0900

    * P202迄の内容で作成したソースを一括でコミット *書籍名:Visual C# 2015 パーフェクトマスター *出版社:秀和システム

---
+++ 20211112_OneDimensionalArray/20211112_OneDimensionalArray/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20211112_OneDimensionalArray
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int[] array = new int[5];
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 frmDialog = new Form2();
+            for (int i = 0; i < 5; i++)
+            {
+                frmDialog.Text = (i + 1) + "番目の要素";
+                frmDialog.ShowDialog();
+                array[i] = Convert.ToInt32(frmDialog.textBox1.Text);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string strResult = "";
+            for (int i = 0; i < 5; i++)
+            {
+                strResult = strResult + Convert.ToString(array[i]) + "\r\n";
+            }
+            textBox1.Text = strResult;
+        }
+    }
+}
