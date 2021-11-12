90c1eceb73fad4c79755212902a26bea5fca8719
tieamap <tieamap@gmail.com>
2021-11-12 11:12:02 +0900

    * P168迄の内容で作成したソースを一括でコミット *書籍名:Visual C# 2015 パーフェクトマスター *出版社:秀和システム

---
+++ 20211112_IfElse/20211112_IfElse/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20211112_IfElse
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked) MessageBox.Show("チェックボックがチェックされています。", "確認");
+            else MessageBox.Show("チェックボックスがチェックされていません。", "確認");
+        }
+    }
+}
