fcb6b086ba8fc34ddcfdfeb6e975083b8f113abd
tieamap <tieamap@gmail.com>
2021-11-12 11:31:55 +0900

    * * P172迄の内容で作成したソースを一括でコミット *書籍名:Visual C# 2015 パーフェクトマスター *出版社:秀和システム

---
+++ 20211112_IfElseIf/20211112_IfElseIf/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20211112_IfElseIf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                MessageBox.Show("今週末は「庭の掃除」をする。", "確認");
+            }
+            else if (radioButton2.Checked)
+            {
+                MessageBox.Show("今週末は「窓拭き」をする。", "確認");
+            }
+            else if (radioButton3.Checked)
+            {
+                MessageBox.Show("今週末は「車の洗車」をする。", "確認");
+            }
+        }
+    }
+}
---
+++ 20211112_SwitchApp/20211112_SwitchApp/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20211112_SwitchApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int score;
+            score = Convert.ToInt32(textBox1.Text);
+
+            switch (score)
+            {
+                case 5:
+                    MessageBox.Show("あなたの成績はAランクです。", "結果");
+                    break;
+                case 4:
+                    MessageBox.Show("あなたの成績はBランクです。", "結果");
+                    break;
+                case 3:
+                    MessageBox.Show("あなたの成績はCランクです。", "結果");
+                    break;
+                default:
+                    MessageBox.Show("あなたの成績はDランクです。", "結果");
+                    break;
+            }
+        }
+    }
+}
