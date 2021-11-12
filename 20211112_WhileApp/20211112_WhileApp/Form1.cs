8348793de6a6cb6d00cb031674df19c814e4c73f
tieamap <tieamap@gmail.com>
2021-11-12 12:00:10 +0900

    * P176迄の内容で作成したソースを一括でコミット *書籍名:Visual C# 2015 パーフェクトマスター *出版社:秀和システム

---
+++ 20211112_WhileApp/20211112_WhileApp/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20211112_WhileApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = 5;
+            while (i != 0)
+            {
+                MessageBox.Show("あと" + Convert.ToString(i) + "回処理を実行します。", "プログラム実行中");
+                i--;
+            }
+        }
+    }
+}
