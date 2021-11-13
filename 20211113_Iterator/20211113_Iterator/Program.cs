ef233ee432bccb170c4056ad7a1d14b336dc83b7
tieamap <tieamap@gmail.com>
2021-11-13 10:26:22 +0900

    * P235迄の内容で作成したソースを一括でコミット *書籍名:Visual C# 2015 パーフェクトマスター *出版社:秀和システム

---
+++ 20211113_Iterator/20211113_Iterator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace _20211113_Iterator
+{
+    class Program
+    {
+        static IEnumerable Show()
+        {
+            yield return "January";
+            yield return "February";
+            yield return "March";
+        }
+
+        static void Main(string[] args)
+        {
+            foreach (string val in Show())
+            {
+                Console.WriteLine(val);
+            }
+        }
+    }
+}
---
+++ 20211113_IteratorDays/20211113_IteratorDays/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace _20211113_IteratorDays
+{
+    class Program
+    {
+        public static IEnumerable Test(int days)
+        {
+            DateTime dt = DateTime.Today;
+            for (int i = 1; i <= days;)
+            {
+                //土曜と日曜を除く日付を返す
+                if (dt.DayOfWeek != DayOfWeek.Sunday && dt.DayOfWeek != DayOfWeek.Saturday)
+                {
+                    yield return dt; //日付を戻り値として返す
+                    i++; //処理回数の加算
+                }
+                dt = dt.AddDays(1); //日付に1日加算
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // Test()を10回呼び戻す
+            foreach (DateTime dt in Test(10))
+            {
+                Console.WriteLine(dt.ToString("yyyy年MM月dd日(ddd)"));
+            }
+        }
+    }
+}
