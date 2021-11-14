8937cdcf5d401ccd687696560bce071a428914bb
tieamap <tieamap@gmail.com>
2021-11-14 12:59:25 +0900

    * P262迄の内容で作成したソースを一括でコミット *書籍名:Visual C# 2015 パーフェクトマスター *出版社:秀和システム

---
+++ 20211114_PrefecturalCode/20211114_PrefecturalCode/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20211114_PrefecturalCode
+{
+    public enum Code : int
+    {
+        Gunma = 10,
+        Saitama = 11,
+        Chiba = 12,
+        Tokyo = 13,
+        Kanagawa = 14
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Code value = Code.Saitama;
+            Disp(value);
+        }
+
+        static void Disp(Code value)
+        {
+            // 渡されたコードによって分岐
+            switch (value)
+            {
+                case Code.Gunma:
+                    Console.WriteLine("群馬県のコード=" + (int)value);
+                    break;
+                case Code.Saitama:
+                    Console.WriteLine("埼玉県のコード=" + (int)value);
+                    break;
+                case Code.Chiba:
+                    Console.WriteLine("千葉県のコード=" + (int)value);
+                    break;
+                case Code.Tokyo:
+                    Console.WriteLine("東京都のコード=" + (int)value);
+                    break;
+                case Code.Kanagawa:
+                    Console.WriteLine("神奈川県のコード=" + (int)value);
+                    break;
+            }
+        }
+    }
+}
---
+++ 20211114_Structure/20211114_Structure/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20211114_Structure
+{
+    public partial class Form1 : Form
+    {
+        public struct Customer
+        {
+            public int Number;
+            public string Name;
+            public int Age;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Customer customer1;
+            customer1.Number = Convert.ToInt32(textBox1.Text);
+            customer1.Name = textBox2.Text;
+            customer1.Age = Convert.ToInt32(textBox3.Text);
+
+            MessageBox.Show("顧客番号:" + customer1.Number + "\n" +
+                "氏名:" + customer1.Name + "\n" +
+                "年齢:" + customer1.Age + "\n",
+                "データが入力されました");
+        }
+    }
+}
