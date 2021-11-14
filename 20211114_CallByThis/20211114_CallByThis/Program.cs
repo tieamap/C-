f175f91e14f6b041efd074539de2db883ca8916b
tieamap <tieamap@gmail.com>
2021-11-14 15:46:45 +0900

    * P319迄の内容で作成したソースを一括でコミット *書籍名:Visual C# 2015 パーフェクトマスター *出版社:秀和システム

---
+++ 20211114_CallByThis/20211114_CallByThis/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20211114_CallByThis
+{
+    class Customer
+    {
+        public string id;
+        public string name;
+        public int age;
+
+        // パラメータを持たないコンストラクター
+        public Customer()
+            : this("-", "-", -1)
+        {
+
+        }
+
+        // 2つのパラメータを持つコンストラクター
+        public Customer(string id, string name)
+            : this(id, name, -1)
+        {
+
+        }
+
+        // 3つのパラメータを持つコンストラクター
+        public Customer(string id, string name, int age)
+        {
+            // 3つのパラメータ値をフィールドに格納
+            this.id = id;
+            this.name = name;
+            this.age = age;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer obj1 = new Customer();
+            Customer obj2 = new Customer("A101", "秀和太郎");
+            Customer obj3 = new Customer("B101", "山田二郎", 28);
+
+            Console.WriteLine(obj1.id + "," + obj1.name + "," + obj1.age);
+            Console.WriteLine(obj2.id + "," + obj2.name + "," + obj2.age);
+            Console.WriteLine(obj3.id + "," + obj3.name + "," + obj3.age);
+        }
+    }
+}
---
+++ 20211114_NonNameArray/20211114_NonNameArray/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20211114_NonNameArray
+{
+    class DataSet
+    {
+        public string s;
+        public int[] n;
+
+        public DataSet(String _s, int[] _n)
+        {
+            s = _s;
+            n = _n;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // インスタンスの作成
+            DataSet ds1 = new DataSet(
+                "配列の値",
+                new int[] { 10, 20, 30, 40, 50 }
+                );
+
+            Console.WriteLine(ds1.s + "= {");
+            foreach (var m in ds1.n)
+            {
+                Console.Write(m + " ");
+            }
+            Console.WriteLine("}");
+        }
+    }
+}
