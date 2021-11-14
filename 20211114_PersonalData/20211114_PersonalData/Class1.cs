8ebcea16175082dab1c573be6b53671ccd183365
tieamap <tieamap@gmail.com>
2021-11-14 14:18:57 +0900

    * P282迄の内容で作成したソースを一括でコミット *書籍名:Visual C# 2015 パーフェクトマスター *出版社:秀和システム

---
+++ 20211114_PersonalData/20211114_PersonalData/Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20211114_PersonalData
+{
+    class Class1
+    {
+        private string name; // 氏名を格納するフィールド
+        private DateTime birthday; // 誕生日を格納するフィールド
+
+        public Class1(string cst_name, DateTime cst_birthday)
+        {
+            Name = cst_name;
+            Birthday = cst_birthday;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if(value == "")
+                {
+                    System.Windows.Forms.MessageBox.Show("名前を入力してください。", "確認");
+                    name = "????";
+                }
+                else
+                {
+                    name = value;
+                }
+            }
+        }
+
+        public DateTime Birthday
+        {
+            get { return birthday; }
+            set
+            {
+                if (value > DateTime.Today)
+                {
+                    System.Windows.Forms.MessageBox.Show("今日以前の日付を選択してください。", "確認");
+                    birthday = DateTime.Today;
+                }
+                else
+                {
+                    birthday = value;
+                }
+            }
+        }
+
+        public int GetAge()
+        {
+            int age = DateTime.Today.Year - birthday.Year;
+            if (
+                DateTime.Today.Month < birthday.Month ||
+                DateTime.Today.Month == birthday.Month &&
+                DateTime.Today.Day < birthday.Day)
+            {
+                age = age - 1;
+            }
+            return age;
+        }
+    }
+}
---
+++ 20211114_PersonalData/20211114_PersonalData/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20211114_PersonalData
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Class1 person = new Class1(textBox1.Text, dateTimePicker1.Value.Date);
+            person.Name = textBox1.Text;
+            person.Birthday = dateTimePicker1.Value.Date;
+            MessageBox.Show(person.Name + "さんの年齢は" + Convert.ToString(person.GetAge()) + "歳です。");
+        }
+    }
+}
