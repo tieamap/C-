ec5fb38fe3323cad6b971d2519e325e72114b46c
tieamap <tieamap@gmail.com>
2021-11-15 22:16:08 +0900

    * P353迄の内容で作成したソースを一括でコミット *書籍名:Visual C# 2015 パーフェクトマスター *出版社:秀和システム

---
+++ 20211115_ReferenceType/20211115_ReferenceType/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20211115_ReferenceType
+{
+    public class TestClass
+    {
+        private int num;
+        public TestClass(int num)
+        {
+            this.num = num;
+        }
+        public int Num
+        {
+            get { return num; }
+        }
+
+        // クラス型のパラメータを持つメソッド
+        public void Show(TestClass a)
+        {
+            // フィールドnumの値をvalueに代入する
+            Console.WriteLine(
+                "呼び出し元のインスタンスのフィールド値は" + this.num);
+            Console.WriteLine(
+                "引数で渡されたインスタンスのフィールド値は" + a.num);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestClass obj1 = new TestClass(100);
+            TestClass obj2 = new TestClass(500);
+            // 参照変数obj2を引数にしてshow()メソッドを呼び出す
+            obj1.Show(obj2);
+        }
+    }
+}
---
+++ 20211115_SuperClassType/20211115_SuperClassType/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20211115_SuperClassType
+{
+    class Customer
+    {
+        public string name;
+        public void registry(string name)
+        {
+            this.name = name;
+        }
+    }
+
+    class Country : Customer
+    {
+        string country;
+        void registry(string name, string country)
+        {
+            this.name = name;
+            this.country = country;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer obj = new Country();
+            obj.registry("Gerry Lopez");
+            Console.WriteLine(obj.name);
+        }
+    }
+}
