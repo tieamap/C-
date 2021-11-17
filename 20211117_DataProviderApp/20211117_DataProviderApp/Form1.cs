217059b5c6a053c1e7c88db3f40bf999c571eadb
tieamap <tieamap@gmail.com>
2021-11-17 21:03:58 +0900

    * P720迄の内容で作成したソースを一括でコミット *書籍名:Visual C# 2015 パーフェクトマスター *出版社:秀和システム

---
+++ 20211117_DataProviderApp/20211117_DataProviderApp/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace _20211117_DataProviderApp
+{
+    public partial class Form1 : Form
+    {
+        // SqlConnectionクラスのインスタンスを作成
+        private SqlConnection cn = new SqlConnection();
+        private SqlCommand cmd = new SqlCommand();
+        private SqlDataReader rd;
+        private string cnstr =
+            @"Data Source = (LocalDB) \MSSQLLocalDB;" +
+            @"AttachDbFilename = |DataDirectory| \Database1.mdf;" +
+            "Integrated Security = True;" +
+            "Connect Timeout = 30";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cn.ConnectionString = cnstr;
+            cn.Open();
+
+            cmd.Connection = cn;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT * FROM [dbo].[Table]";
+
+            rd = cmd.ExecuteReader();
+            while (rd.Read())
+                listBox1.Items.Add(
+                    String.Format("[{0}] {1, -10} {2, -10} {3, -30} 【Tel】{4}",
+                        rd["ID"], rd["Name"], rd["ZipCode"],
+                        rd["Address"], rd["Tel"]));
+
+            rd.Close();
+            cn.Close();
+        }
+    }
+}
